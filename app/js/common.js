6bb476a602c73e6f43767eafbebafea8cc382549
leonidlist <list.leonid@gmail.com>
2019-08-16 19:16:15 +0300

    Setup basic layout

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,2 @@
-$(function() {
-
-	// Custom JS
-
-});
+// var apiKey = "70317cafb764db84a165ea51cf5c045c";
+// var oneDayForecastTemplate = `https://api.openweathermap.org/data/2.5/weather?q=${city},uk&appid=${apiKey}`;
