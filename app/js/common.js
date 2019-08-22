32bff873cab26a883138af5748ebbb2794154287
Leonid List <list.leonid@gmail.com>
2019-08-22 17:31:10 +0300

    Tilt

--- app/js/common.js
+++ app/js/common.js
@@ -1,2 +1,9 @@
-// var apiKey = "70317cafb764db84a165ea51cf5c045c";
-// var oneDayForecastTemplate = `https://api.openweathermap.org/data/2.5/weather?q=${city},uk&appid=${apiKey}`;
+var apiKey = "70317cafb764db84a165ea51cf5c045c";
+var country = "ua";
+var oneDayForecastTemplate = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`;
+
+$(document).ready(function() {
+    $.get(oneDayForecastTemplate, (data) => {
+        
+    })
+})
